b1608b5de73cfb7f8a9eac4554846e81c5c58853
Diego Nul <nuldiego@gmail.com>
2013-05-18 19:01:15 -0300

    inital facebook login

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,23 @@
 package controllers;
 
+import models.User;
+
+import com.feth.play.module.pa.PlayAuthenticate;
+import com.feth.play.module.pa.user.AuthUser;
+
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Http.Session;
 import views.html.index;
 
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+    	
+    	AuthUser user = PlayAuthenticate.getUser(session());
+    	User appUser = User.findByAuthUserIdentity(user);
+    	
+        return ok(index.render("Your new application is ready.",appUser));
     }
     
     public static Result oAuthDenied(final String providerKey) {
@@ -15,5 +25,11 @@ public class Application extends Controller {
                 "You need to accept the OAuth connection in order to use this website!");
         return redirect(routes.Application.index());
     }
+    
+	public static User getLocalUser(final Session session) {
+		final AuthUser currentAuthUser = PlayAuthenticate.getUser(session);
+		final User localUser = User.findByAuthUserIdentity(currentAuthUser);
+		return localUser;
+	}
   
 }
