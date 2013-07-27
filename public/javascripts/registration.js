2f054fca0100af434469bea4b4db920214532a75
Lucas Derossi <lucas@Lucas-PC.linksys.com>
2013-07-27 16:25:25 -0300

    Registration js refactor . Added maps

--- public/javascripts/registration.js
+++ public/javascripts/registration.js
@@ -34,7 +34,7 @@ YORECICLO.Register = (function () {
 	return {
 		//public scope
 		successRegistration: function (){
-			console.log(profileData);
+			//console.log(profileData);
 		},
 		subMission: function (elem,fullProfile,event){
 			var formContainer = $(elem).parent();
@@ -62,5 +62,68 @@ YORECICLO.Register = (function () {
 		},
 		mapCoordenates: []
 	}
+})();
+
+YORECICLO.Maps = (function (){
+
+	return{
+			app : {},
+			initializeMap : function (destination) {
+				var mapOptions = {
+					center : new google.maps.LatLng(-34.602128,-58.430895),
+					zoom : 10,
+					mapTypeId : google.maps.MapTypeId.ROADMAP
+				};
+				var map = new google.maps.Map(document.getElementById(destination),
+						mapOptions);
+				
+				this.app.map=map;
+			},
+			
+			initRegitrationMap: function () {
+				var ac = new usig.AutoCompleter('address', {
+	       			rootUrl: 'http://servicios.usig.buenosaires.gob.ar/usig-js/2.3/',
+	       			skin: 'usig4',
+	       			onReady: function() {
+	       				$('#address').val('').focus();
+	       			},
+	       			afterSelection: function(option) {
+	       				//console.log('Se seleccionó la opción: '+option);
+	       			},
+					afterGeoCoding : function(pt) {
+						if (pt instanceof usig.Punto) {
+							$.ajax({
+								type : "GET",
+								url : 'http://ws.usig.buenosaires.gob.ar/rest/convertir_coordenadas?x=' + pt.x +'&y=' + pt.y + '&output=lonlat',
+								data : null,
+								dataType: 'jsonp',
+								success : function(d) {
+									var myLatlng = new google.maps.LatLng(d.resultado.y,d.resultado.x);
+									//myLatlng = new google.maps.LatLng(-34.602128,-58.430895);
+									$("#map-registration").show();
+									YORECICLO.Maps.initializeMap("map-registration");
+									var marker = new google.maps.Marker({
+									      position: myLatlng,
+									      map: YORECICLO.Maps.app.map,
+									      title: 'ubicación'
+									  });
+									YORECICLO.Maps.app.map.setCenter(marker.getPosition());
+									YORECICLO.Maps.app.map.setZoom(15);
+									YORECICLO.Register.mapCoordenates = ([d.resultado.y,d.resultado.x]);
+								
+								},
+								error : null
+							});
+						}
+					}
+				});
+			ac.addSuggester('Catastro', {
+				inputPause : 200,
+				minTextLength : 1,
+				showError : false
+			});
+			}
+	}
+
 })();
 
